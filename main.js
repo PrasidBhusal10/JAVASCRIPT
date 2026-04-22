b308c332ff1c916538446ff975ae7cac2ed41bf8
PrasidBhusal10 <prasidbhusal07@gmail.com>
2026-04-22 17:48:34 -0500

    update

---
+++ main.js
@@ -0,0 +1,88 @@
+// in-line comment
+/* multi-line comment
+hi this is the multi-line comment
+
+
+-----------------------------------------------------------
+
+/* DATA TYPES
+
+we can declare variable by three types: var, let, const.
+var can be change but const can't be change
+
+-----------------------------------------------------------------------
+
+
+/* To printing in javascript
+var a = 7;
+console.log(a)
+----------------------------------------------------------------------------
+
+
+
+/* ARITHMATIC OPERATION
+var sum = 1 + 5;
+var diff = 6 -  2;
+var multi = 2 * 2;
+var div = 6 / 2;
+var quotient = 6 % 2;
+console.log(sum, diff, multi, div, quotient);
+
+-------------------------------------------------------
+
+/* Augmented addition 
+ var  a = 3;
+ a += 12; is same as   a = a + 12; (same for -=, *=, /=)
+ console.log(a)
+
+ ------------------------------------------------------------
+
+ /* STRING
+ var myname = 'prasid';
+ console.log(myname) 
+
+
+----------------------------------------------------------------
+ /* Some feature
+//backlash
+\n newline 
+\r carriage return
+\t tab
+\b backspace
+\f form feed 
+
+ var a = 'prasid \nBhusal \f maya';
+ console.log(a)
+
+-------------------------------------------------------------------- 
+ /* CONCATENATION OF STRING
+ We can concatenate with the + sign
+  var x = 'My name is prasid ' + ' Bhusal. I am from pipara kapilvastu '
+  console.log(x)
+
+  OR
+  var x = 'My name is prasid Bhusal';
+  x += ' I am from pipara kapilvastuuu';
+  console.log(x)
+  
+  */
+
+/* Length of the string 
+var name = 'Prasid'
+console.log(name.length) 
+
+*/
+
+/* first letter
+var a ='amy'
+console.log(a[0])
+
+*/
+
+/* Changing name, we cant change the specific character
+var name = 'prasid'
+name = 'Bhusal';
+console.log(name)
+
+*/
+ 
